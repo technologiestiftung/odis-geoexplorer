4180e63f09a22d7d915296ad190d9ba80898e160
hhack <hans.f.hack@googlemail.com>
2024-03-22 08:10:42 +0100

    fix filter bug

--- components/DatasetInfoRow.tsx
+++ components/DatasetInfoRow.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react'
 
 import { DatasetInfoExtended } from '@/components/DatasetInfoExtended'
-
 import { SimilarityRating } from '@/components/SimilarityRating'
 
 function parseEmbeddingContent(text) {
@@ -41,53 +40,52 @@ export function DatasetInfoRow(props) {
 
   const [showExtraInfo, setShowExtraInfo] = useState<boolean>(false)
   let content = result && result.content ? parseEmbeddingContent(result.content) : {}
-  content['rawContent'] = result.content
+  // content['rawContent'] = result.content
 
   // Return the JSX for your component
   return (
     <>
       {typeFilterValue.includes(content['Typ']) && (
-        <tr key={result.id}>
-          <td className="flex p-4 pl-6">
-            <div
-              style={{ borderStyle: 'solid', outlineWidth: '1px' }}
-              className=" font-bold  outline outline-odis-dark rounded-full p-1 px-2 h-max mr-4 self-center text-xs"
-            >
-              {content['Typ']}
-            </div>
-            <span className="">
-              {content['Titel'].replace(' - \\[WMS]', '').replace(' - \\[WFS]', '')}
-            </span>
-          </td>
-          <td className="p-4">
-            <SimilarityRating similarity={result.similarity}></SimilarityRating>
-          </td>
-          <td className="p-4">
-            <button
-              onClick={() => {
-                setShowExtraInfo(!showExtraInfo)
-              }}
-              type="submit"
-              className="text-white text-xs  rounded-md w-20 px-1 py-1 hover:bg-active bg-odis-light "
-            >
-              Mehr Infos
-            </button>
-          </td>
-        </tr>
-      )}
+        <>
+          <tr key={result.id}>
+            <td className="flex p-4 pl-6">
+              <div className=" font-bold  border !border-odis-dark rounded-full p-1 px-2 h-max mr-4 self-center text-xs">
+                {content['Typ']}
+              </div>
+              <span className="">
+                {content['Titel'].replace(' - \\[WMS]', '').replace(' - \\[WFS]', '')}
+              </span>
+            </td>
+            <td className="p-4">
+              <SimilarityRating similarity={result.similarity}></SimilarityRating>
+            </td>
+            <td className="p-4">
+              <button
+                onClick={() => {
+                  setShowExtraInfo(!showExtraInfo)
+                }}
+                type="submit"
+                className="text-white text-xs  rounded-md w-20 px-1 py-1 hover:bg-active bg-odis-light "
+              >
+                Mehr Infos
+              </button>
+            </td>
+          </tr>
 
-      {showExtraInfo && (
-        <tr colSpan={3}>
-          <td colSpan={3} className="">
-            <DatasetInfoExtended
-              contentDataset={content}
-              inputText={inputText}
-              setSimilarSearchText={setSimilarSearchText}
-            />
-          </td>
-        </tr>
+          {showExtraInfo && (
+            <tr colSpan={3}>
+              <td colSpan={3} className="">
+                <DatasetInfoExtended
+                  contentDataset={content}
+                  inputText={inputText}
+                  setSimilarSearchText={setSimilarSearchText}
+                />
+              </td>
+            </tr>
+          )}
+          <tr className="border-b-[1px]" colSpan={3}></tr>
+        </>
       )}
-      <tr className="border-b-[1px]" colSpan={3}></tr>
     </>
   )
 }
