e8c87a2ca1dcf67eb9a5cdbcd99d871621a0228b
hhack <hans.f.hack@googlemail.com>
2024-04-03 06:59:55 +0200

    links to modal

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -41,38 +41,66 @@ export function InfoModal({ language }) {
               </span>
             </div>
           </DialogTitle>
-          <DialogDescription className="text-black text-md">
-            Entdecke Berlins Geodaten mit GeoExplore Berlin. Mit diesem Prototyp kannst du spielend
-            leicht Daten aus mehr als 650 Geodatensätze aus dem{' '}
-            <a
-              target="_blank"
-              href="https://www.berlin.de/sen/sbw/stadtdaten/geoportal/"
-              className="text-odis-light"
-            >
-              Geodatenportal Berlins
-            </a>{' '}
-            finden. Die Anwendung nutzt ein Large Language Model, um gezielt nach interessanten
-            Datensätzen im WFS-Format für dich zu suchen, die sonst oft nur mit technischem know-how
-            zugänglich sind. Egal, ob Umwelt, Infrastruktur oder Kultur – finde die Daten, die dich
-            interessieren.
-            <br />
-            <br />
-            <b>Hinweise zur Nutzung</b>
-            <br />
-            <span className="mt-4">
+          <DialogDescription className="text-odis-dark">
+            <p className="text-lg font-bold leading-6 mb-4">
+              Entdecke Berlins Geodaten mit GeoExplore Berlin. Mit diesem Prototyp kannst du
+              spielend leicht Daten aus mehr als 650 Geodatensätze aus dem{' '}
+              <a
+                target="_blank"
+                href="https://www.berlin.de/sen/sbw/stadtdaten/geoportal/"
+                className="text-odis-light"
+              >
+                Geodatenportal Berlins
+              </a>{' '}
+              finden. Die Anwendung nutzt ein Large Language Model, um gezielt nach passenden
+              Datensätzen für dich zu suchen, die sonst oft nur mit technischem know-how zugänglich
+              sind. Egal, ob Umwelt, Infrastruktur oder Kultur – finde die Daten, die dich
+              interessieren.
+            </p>
+            <p>
+              <span className="font-bold">Hinweise zur Nutzung</span>
+              <br />
               Die auf dieser Website präsentierten Ergebnisse basieren auf einem Large Language
               Model und dienen ausschließlich zu Informationszwecken. Es ist wichtig zu beachten,
               dass solche Modelle Fehler machen können und gelegentlich “halluzinieren”, d.h.
               erdachte Informationen generieren können. Daher sollten die bereitgestellten
               Informationen mit Vorsicht verwendet werden.
-            </span>
-            <br />
-            <span className="mt-4">
+            </p>
+            <p className="pt-4">
               Wir übernehmen keine Haftung für die Richtigkeit, Vollständigkeit oder Aktualität der
               auf dieser Website präsentierten Daten. Nutzerinnen und Nutzer werden ermutigt, die
               bereitgestellten Informationen zu überprüfen und im Zweifelsfall auf offizielle
               Quellen zurückzugreifen.
-            </span>
+            </p>
+            <p className="flex justify-start gap-x-6 gap-y-3 mt-4 flex-wrap font-bold text-odis-light">
+              <a
+                href="https://www.technologiestiftung-berlin.de/de/datenschutz/"
+                target="_blank"
+                className="flex-1"
+              >
+                Datenschutz
+              </a>
+              <a href="" target="_blank" className="flex-1">
+                Feedback
+              </a>
+              <a href="" target="_blank" className="flex-1">
+                GitHub
+              </a>
+              <a
+                href="https://www.technologiestiftung-berlin.de/de/stiftung/kontakt-anfahrt/"
+                target="_blank"
+                className="flex-1"
+              >
+                Kontakt
+              </a>
+              <a
+                href="https://www.technologiestiftung-berlin.de/de/impressum/"
+                target="_blank"
+                className="flex-1"
+              >
+                Impressum
+              </a>
+            </p>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
