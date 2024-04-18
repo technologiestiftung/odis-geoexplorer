1aa7b21ad3942c004c2bed35bf027b0189748ed6
hhack <hans.f.hack@googlemail.com>
2024-04-18 15:15:03 +0200

    new intro text

--- components/IntroText.tsx
+++ components/IntroText.tsx
@@ -7,7 +7,9 @@ export function IntroText({ language }) {
       <h1 className="text-2xl font-bold">
         Geo<span className="text-odis-light">Explorer</span>
       </h1>
-      <h2 className="">{text[language].subHeading || text['de'].subHeading}</h2>
+      {/* <h2 className="">{text[language].subHeading || text['de'].subHeading}</h2> */}
+      <h2>Finde und erkunde das große Angebot von offenen Geodaten in Berlin</h2>
+      <h2>mit Hilfe von K.I.</h2>
     </div>
   )
 }
