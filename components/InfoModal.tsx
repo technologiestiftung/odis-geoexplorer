d5830f5b9376110afa00f23e87c5a0a31f90e4f4
hhack <hans.f.hack@googlemail.com>
2024-05-14 13:50:35 +0200

    new modal text

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -44,18 +44,15 @@ export function InfoModal({ language }) {
         </div>
         <div className="md:overflow-y-auto">
           <p className="text-lg font-bold leading-6 mb-4">
-            Entdecke Berlins Geodaten mit dem GeoExplorer Berlin. Mit diesem Prototyp kannst du
-            spielend leicht Daten aus mehr als 1800 Geodatensätze aus dem{' '}
-            <a
-              target="_blank"
-              href="https://www.berlin.de/sen/sbw/stadtdaten/geoportal/"
-              className="text-odis-light"
-            >
-              Geodatenportal Berlins
-            </a>{' '}
-            finden. Die Anwendung nutzt ein Large Language Model, um gezielt nach passenden
-            Datensätzen für dich zu suchen, die die KI mit deinem Suchebgriff verbindet. Egal, ob
-            Umwelt, Infrastruktur oder Kultur – finde die Daten, die dich interessieren.
+            Du möchtest die Folgen des Klimawandels für Bäume oder Vögel verstehen? Du planst den
+            Verkehr der Zukunft oder setzt dich für leichtere Teilhabe an Gesundheitseinrichtungen
+            oder Bildungsangeboten ein? In den offenen Geodatensätzen des Landes Berlin schlummert
+            eine Vielzahl an Wissen über unsere Stadt, das dir bei deinem Vorhaben helfen kann.
+            Unser prototypischer GeoExplorer sucht dir basierend auf deiner Anfrage dank
+            KI-Unterstützung passende oder naheliegende Datensätze für dein Projekt. Zusätzlich
+            kannst du in jeden Datensatz genauer eintauchen und nach deinen Bedürfnissen
+            weiternutzen. Egal, ob Umwelt, Infrastruktur oder Kultur – exploriere die Daten, die
+            dich interessieren.
           </p>
           <div className="relative bg-odis-extra-light text-odis-light border-odis-light mt-8 overflow-auto rounded-md border border-input p-4">
             <span className="absolute">
@@ -64,10 +61,19 @@ export function InfoModal({ language }) {
             <p className="font-bold pl-6">Hinweis zum KI-Modell</p>
             <br />
             <p className="pl-6">
-              Bitte beachte, dass der GeoExplorer das KI-Modell von ChatGPT nutzt, d.h. Anfragen
-              werden an OpenAI weitergeleitet, die das Unternehmen nach eigenen
-              Datenschutzrichtlinien verarbeitet. Darüber hinaus fallen bei jeder Abfrage Kosten an.
-              Strom Verbrauch?
+              Bitte beachte, dass der GeoExplorer das KI-Modell von ChatGPT nutzt, das heißt
+              Anfragen werden an OpenAI weitergeleitet, die das Unternehmen nach eigenen{' '}
+              <a
+                target="_blank"
+                className="underline"
+                href="https://openai.com/policies/privacy-policy"
+              >
+                Datenschutzrichtlinien
+              </a>{' '}
+              verarbeitet. Eine Speicherung der Anfragen durch uns erfolgt nicht. Darüber hinaus
+              fallen bei jeder Abfrage geringfügige Kosten aus Mitteln der öffentlichen Hand für die
+              Nutzung von ChatGPT an und jede Anfrage ist mit einem erhöhten Stromverbrauch im
+              Vergleich zu üblichen Suchmaschinen verbunden.
             </p>
           </div>
           <div className=" relative bg-odis-extra-light text-odis-light border-odis-light mt-8 overflow-auto rounded-md border border-input p-4">
@@ -77,17 +83,16 @@ export function InfoModal({ language }) {
             <span className="font-bold flex pl-6">Hinweise zur Nutzung</span>
             <br />
             <p className="ml-6">
-              Die auf dieser Website präsentierten Ergebnisse basieren auf einem Large Language
-              Model und dienen ausschließlich zu Informationszwecken. Es ist wichtig zu beachten,
-              dass KI-Modelle gelegentlich Informationen ausgeben, die zwar plausibel klingen aber
-              dennoch falsch sein können. Daher sollten die bereitgestellten Informationen mit
-              Vorsicht betrachtet werden.
+              Die auf dieser Website präsentierten Ergebnisse sind abhängig von der konkreten
+              Suchanfrage (dem Prompt). Es ist wichtig zu beachten, dass Large Language Models wie
+              ChatGPT gelegentlich Informationen ausgeben, die zwar plausibel klingen aber dennoch
+              falsch sein können. Daher sollten die bereitgestellten Informationen mit Vorsicht
+              betrachtet werden.
               <br />
               <br />
               Wir übernehmen keine Haftung für die Richtigkeit, Vollständigkeit oder Aktualität der
-              auf dieser Website präsentierten Daten. Nutzerinnen und Nutzer werden ermutigt, die
-              bereitgestellten Informationen zu überprüfen und im Zweifelsfall auf offizielle
-              Quellen zurückzugreifen.
+              auf dieser Website präsentierten Daten und empfehlen die bereitgestellten
+              Informationen zu überprüfen.
             </p>
           </div>
           <Accordion
@@ -97,7 +102,7 @@ export function InfoModal({ language }) {
             }
           />
           <p className="mt-6">
-            Letzter Abgleich mit dem{' '}
+            Letzter Datenabgleich mit dem{' '}
             <a className="text-odis-light" href="https://daten.berlin.de/datensaetze">
               Berliner Datenportal
             </a>
