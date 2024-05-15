971acbf0e27213829b8e58e51bb026d4e1ff0ff2
hhack <hans.f.hack@googlemail.com>
2024-05-15 16:18:39 +0200

    add checks again for similarity

--- components/SimilarityRating.tsx
+++ components/SimilarityRating.tsx
@@ -3,25 +3,25 @@
 import { CheckIcon } from '@/components/ui/icons/check'
 
 export function SimilarityRating({ similarity }) {
-  // let activeIcons = 0
+  let activeIcons = 0
 
-  // if (similarity < 0.8) {
-  //   activeIcons = 1
-  // } else if (similarity >= 0.8 && similarity < 0.84) {
-  //   activeIcons = 2
-  // } else if (similarity >= 0.84) {
-  //   activeIcons = 3
-  // }
+  if (similarity < 0.8) {
+    activeIcons = 1
+  } else if (similarity >= 0.8 && similarity < 0.84) {
+    activeIcons = 2
+  } else if (similarity >= 0.84) {
+    activeIcons = 3
+  }
 
-  // return (
-  //   <div className="flex">
-  //     {[...Array(3)].map((_, index) => (
-  //       <span key={'sim' + index} className={index < activeIcons ? '' : 'opacity-20'}>
-  //         <CheckIcon key={index} />
-  //       </span>
-  //     ))}
-  //   </div>
-  // )
+  return (
+    <div className="flex">
+      {[...Array(3)].map((_, index) => (
+        <span key={'sim' + index} className={index < activeIcons ? '' : 'opacity-20'}>
+          <CheckIcon key={index} />
+        </span>
+      ))}
+    </div>
+  )
 
   // if (similarity < 0.8) {
   //   return (
@@ -96,9 +96,9 @@ export function SimilarityRating({ similarity }) {
   //   )
   // }
 
-  return (
-    <span className="font-bold text-odis-light">
-      {similarity < 0.8 ? 'niedrig' : similarity >= 0.8 && similarity < 0.84 ? 'mittel' : 'hoch'}
-    </span>
-  )
+  // return (
+  //   <span className="font-bold text-odis-light">
+  //     {similarity < 0.8 ? 'niedrig' : similarity >= 0.8 && similarity < 0.84 ? 'mittel' : 'hoch'}
+  //   </span>
+  // )
 }
